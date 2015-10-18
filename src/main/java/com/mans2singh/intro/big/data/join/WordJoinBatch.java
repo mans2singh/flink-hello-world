6587d352efe7eb7fadcb35bd90a9596a0c6facf9
msingh2 <mansnimar_singh@intuit.com>
2015-10-18 08:13:19 -0700

    Join example

---
+++ src/main/java/com/mans2singh/intro/big/data/join/WordJoinBatch.java
@@ -0,0 +1,53 @@
+package com.mans2singh.intro.big.data.join;
+
+import java.util.Arrays;
+
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.JoinOperator.DefaultJoin;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.mans2singh.intro.big.data.WordMapper;
+import com.mans2singh.intro.big.data.WordSplitter;
+
+public class WordJoinBatch {
+	
+	protected final static Logger LOGGER = LoggerFactory.getLogger(WordJoinBatch.class);
+	
+	public static void main(String[] args) throws Exception {
+
+		LOGGER.info("Arguments were " + Arrays.toString(args));
+		
+		if ( args.length != 2 ) {
+			LOGGER.error("Please enter 2 filenames");
+			return;
+		}
+		
+		// Create an environment
+		final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+		// Create a data set
+		DataSet<String> firstWords = env.readTextFile(args[0]);
+		DataSet<String> secondWords = env.readTextFile(args[1]);
+				
+		// Process the data set for word count
+		DataSet<Tuple2<String, Integer>> firstCounts = firstWords.flatMap(new WordSplitter()).map(new WordMapper())
+				.groupBy(0)
+				.sum(1);
+
+		DataSet<Tuple2<String, Integer>> secondCounts = secondWords.flatMap(new WordSplitter()).map(new WordMapper())
+				.groupBy(0)
+				.sum(1);
+
+		// Join the two data sets
+		DefaultJoin<Tuple2<String, Integer>, Tuple2<String, Integer>> joinResult = 
+				firstCounts.join(secondCounts).where(0).equalTo(0);
+		
+		// Print the count
+		joinResult.print();
+		
+	}
+
+}
