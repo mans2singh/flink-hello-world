dc9c8605dc9f8086149eb44c7ca9ae0123f24c4d
msingh2 <mansnimar_singh@intuit.com>
2015-10-17 22:13:57 -0700

    first version of word splitter

---
+++ src/main/java/com/mans2singh/intro/big/data/WordSplitter.java
@@ -0,0 +1,33 @@
+package com.mans2singh.intro.big.data;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.util.Collector;
+
+/**
+ * Function to split a line into words
+ * @author msingh2
+ */
+public class WordSplitter implements FlatMapFunction<String,String> {
+
+	/**
+	 * Serial version of the class
+	 */
+	private static final long serialVersionUID = -3934658156048663316L;
+	
+	/**
+	 * Split token for words
+	 */
+	private static final String WORD_SPLIT = " ";
+
+	/**
+	 * Split line into words separated by space
+	 */
+	@Override
+	public void flatMap(String line, Collector<String> collector) throws Exception {
+		String [] words = line.split(WORD_SPLIT);
+		for (String word: words) {
+			collector.collect(word);
+		}
+	}
+
+}
